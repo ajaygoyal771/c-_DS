dddb07f2fda37b61608c7b5dbd9506214eb62a73
Ajay Goyal <ajaygoyal771@gmail.com>
2017-06-05 18:50:22 +0530

    Update and rename cc_ATM to cc_ATM.cpp

---
+++ cc_ATM.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdio>
+
+int main()
+{
+
+float x,y,z;
+
+scanf("%f %f",&x,&y);
+if(0<x<=2000 && x%5=0)
+{
+z=y-x-0.50;
+printf("%0.2f",z);
+}
+else;
+{
+printf("%0.2f",y);;
+}
+return 1;
+}
